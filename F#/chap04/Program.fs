d9540359320e6a09cea6d67826937b2b0d1350d0
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2024-12-29 11:41:17 +0900

    Add examples of basic functions in Chapter 4.1
    Introduced several new functions including `add1`, `add`, `squarePlusOne`, and `areEqual`, each with example usages. These updates enhance the demonstration of fundamental concepts in F#.

--- F#/chap04/Program.fs
+++ F#/chap04/Program.fs
@@ -1,2 +1,19 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿printfn "Chapter 4.1"
+let add1 x = x + 1
+
+printfn $"ex) add1 2 -> {add1 2}"
+
+let add x y = x + y
+
+printfn $"ex) add 2 4 -> {add 2 4}"
+
+let squarePlusOne x =
+    let square = x * x
+    square + 1
+
+printfn $"ex) squarePlusOne 3 -> {squarePlusOne 3}"
+
+let areEqual x y = (x = y)
+
+printfn $"ex) areEqual 2 3 -> {areEqual 2 3}"
+printfn $"ex) areEqual 4.2 4.2 -> {areEqual 4.2 4.2}"
