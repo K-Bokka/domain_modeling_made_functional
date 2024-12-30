51e0a0eee65ef1ec61db6a32c60abc428d361183
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2024-12-30 16:17:07 +0900

    Add examples and utility functions for Chapter 4.1
    Refactored `main.scala` to include demonstrations of new functions and introduced `Chap0401.scala` with basic utility methods. These functions cover addition, squaring, and equality checks to support Chapter 4.1 examples.

---
+++ Scala/chap04/src/main/scala/Chap0401.scala
@@ -0,0 +1,12 @@
+object Chap0401 {
+  def add1(x: Int): Int = x + 1
+
+  def add(x: Int, y: Int): Int = x + y
+
+  def squarePlusOne(x: Int): Int =
+    def square(x: Int): Int = x * x
+
+    square(x) + 1
+
+  def areEqual[T](x: T, y: T): Boolean = x == y
+}
--- Scala/chap04/src/main/scala/main.scala
+++ Scala/chap04/src/main/scala/main.scala
@@ -1,5 +1,8 @@
 @main def hello(): Unit =
-  println("Hello world!!")
-  println(msg)
-
-def msg = "I was compiled by Scala 3. ;)"
+  println("Chapter 4.1")
+  println(s"ex) add1 2 -> ${Chap0401.add1(2)}")
+  println(s"ex) add 2 4 -> ${Chap0401.add(2, 4)}")
+  println(s"ex) squarePlusOne 3 -> ${Chap0401.squarePlusOne(3)}")
+  println(s"ex) areEqual 2 3 -> ${Chap0401.areEqual(2, 3)}")
+  println(s"ex) areEqual 4.2 4.2 -> ${Chap0401.areEqual(4.2, 4.2)}")
+  println()
