8118e53d7d15458c0feae1f000c5b1ee717eba22
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-01-02 10:02:38 +0900

    Add domain modeling types and examples to Chapter 5.3
    Introduced several domain-specific types in a new `Chap0503.fs` file, including `CustomerId`, `OrderId`, and `UnitQuantity`. Updated `Program.fs` to demonstrate their use and added the new file to `chap05.fsproj`.

--- F#/chap05/Program.fs
+++ F#/chap05/Program.fs
@@ -1,2 +1,21 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Chap0503.C050301
+
+printfn "Chapter 5.3"
+
+let customerId = CustomerId 42
+let orderId = OrderId 42
+
+// println $"%b{orderId = customerId}"
+printfn """
+./domain_modeling_made_functional/F#/chap05/Program.fs(15,25): error FS0001: この式に必要な型は    'OrderId'    ですが、ここでは次の型が指定されています    'CustomerId'
+"""
+
+let processCustomerId (id: CustomerId) = printfn $"Customer ID is {id}"
+
+// processCustomerId orderId
+printfn """
+./domain_modeling_made_functional/F#/chap05/Program.fs(27,19): error FS0001: この式に必要な型は    'CustomerId'    ですが、ここでは次の型が指定されています    'OrderId'
+"""
+
+let (CustomerId innerValue) = customerId
+printfn $"innerValue is {innerValue}"
