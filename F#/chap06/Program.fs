a04deeb65e3838e98460117fb4f6e9e53ed13454
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-01-11 21:01:50 +0900

    Add Chapter 6 project with unit quantity domain logic
    Introduced a new `chap06` project with F# implementation for chapter 6. This includes a private `UnitQuantity` type with validation logic and helper functions. Updated the solution file to include the new project.

---
+++ F#/chap06/Program.fs
@@ -0,0 +1,20 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open Chap0601
+
+printfn "Chapter 6.1"
+
+// let unitQty = UnitQuantity 1
+printfn
+    """
+./domain_modeling_made_functional/F#/chap06/Program.fs(7,15): error FS1093: 型 'UnitQuantity' の共用体ケースまたはフィールドは、このコードの場所からアクセスできません
+"""
+
+let unitQtyResult = UnitQuantity.create 1
+
+match unitQtyResult with
+| Error msg -> printfn $"Failure. Message is {msg}"
+| Ok uQty ->
+    printfn $"Success. Value is {uQty}"
+    let innerValue = UnitQuantity.value uQty
+    printfn $"inner Value is {innerValue}"
