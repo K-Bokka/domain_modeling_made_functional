bfde47f8ca5a47b7310278e568df05d06dfef787
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-01-11 21:11:37 +0900

    Add unit of measure types and integrate Chap0602 module
    Introduced `kg` and `m` unit-of-measure types in the new `Chap0602` module. Updated `Program.fs` to demonstrate the use of these types and included the module in the project file for compilation.

--- F#/chap06/Program.fs
+++ F#/chap06/Program.fs
@@ -1,6 +1,7 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 
 open Chap0601
+open Chap0602
 
 printfn "Chapter 6.1"
 
@@ -18,3 +19,16 @@ match unitQtyResult with
     printfn $"Success. Value is {uQty}"
     let innerValue = UnitQuantity.value uQty
     printfn $"inner Value is {innerValue}"
+
+printfn "\nChapter 6.2"
+
+let fiveKilos = 5.0<kg>
+let fiveMeters = 5.0<m>
+
+// let res = fiveKilos = fiveMeters
+printfn """
+./domain_modeling_made_functional/F#/chap06/Program.fs(28,13): error FS0001: 型が一致しません。    'float<kg>'    という指定が必要ですが、    'float<m>'    が指定されました。測定単位 'm' と一致しません
+"""
+
+// コンパイルエラーにならないな...
+let listOfWeights = [fiveKilos, fiveMeters]
