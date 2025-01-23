c9cd4d2cd149418b34cbff3df52bd80163d69ac4
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-01-24 06:33:22 +0900

    Transcribed Chapter 8.2.1 in Scala

--- Scala/chap08/src/main/scala/main.scala
+++ Scala/chap08/src/main/scala/main.scala
@@ -1,2 +1,20 @@
 @main def main(): Unit =
   println("Chapter 8")
+
+  C080201.listFunctions foreach :
+    fn =>
+      val result = fn(100)
+      println(s"If 100 is the input, the output is $result")
+
+
+object C080201:
+  val plus3: Int => Int = (x: Int) => x + 3
+
+  val times2: Int => Int = (x: Int) => x * 2
+
+//  val square: Int => Int = (x: Int) => x * x
+  def square(x: Int): Int = x * x // 上と同じ。ScalaはLambda式とメソッドでは書き方が違う
+
+  val addThree: Int => Int = plus3
+
+  val listFunctions: List[Int => Int] = List(addThree, times2, square)
