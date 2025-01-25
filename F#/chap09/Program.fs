a8903220e2f2a11f26d005d6f3fd8ead0cf9e731
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-01-25 13:24:42 +0900

    Transcribed Chap9.1 in F#

--- F#/chap09/Program.fs
+++ F#/chap09/Program.fs
@@ -1,2 +1,30 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿printfn "Chapter 9"
+
+module Chap9 =
+    let validateOrder _ = failwith "Undefined"
+    let priceOrder _ = failwith "Undefined"
+    let acknowledgeOrder _ = failwith "Undefined"
+    let createEvents _ = failwith "Undefined"
+    
+    let placeOrder unvalidatedOrder =
+        unvalidatedOrder
+        |> validateOrder
+        |> priceOrder
+        |> acknowledgeOrder
+        |> createEvents
+
+module Domain =
+    open System
+
+    type OrderId = private OrderId of string
+
+    module OrderId =
+        let create str =
+            if String.IsNullOrEmpty(str) then
+                failwith "OrderId must not be null or empty"
+            elif str.Length > 50 then
+                failwith "OrderId must not be more than 50 chars"
+            else
+                OrderId str
+
+        let value (OrderId str) = str
