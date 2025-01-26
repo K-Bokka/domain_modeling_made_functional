7d381ab2776041d04dd1a8d9cb53a76c4536029b
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-01-26 11:06:01 +0900

    Transcribed 9.3 in F#

--- F#/chap09/Program.fs
+++ F#/chap09/Program.fs
@@ -1,5 +1,7 @@
 ﻿printfn "Chapter 9"
 
+open System
+
 module Chap9 =
     let validateOrder _ = failwith "Undefined"
     let priceOrder _ = failwith "Undefined"
@@ -13,8 +15,20 @@ module Chap9 =
         |> acknowledgeOrder
         |> createEvents
 
+module Common =
+    type String50 = private String50 of string
+
+    module String50 =
+        let create str =
+            if String.IsNullOrEmpty(str) then
+                failwith "String must not be null or empty"
+            elif str.Length > 50 then
+                failwith "String must not be more than 50 chars"
+            else
+                String50 str
+
 module Domain =
-    open System
+    open Common
 
     type OrderId = private OrderId of string
 
@@ -28,6 +42,51 @@ module Domain =
                 OrderId str
 
         let value (OrderId str) = str
+
+    // For chap0903
+    type Address = private Address of String
+    type EmailAddress = private EmailAddress of String
+
+    module EmailAddress =
+        let value (EmailAddress str) = str
+        let create email = EmailAddress email
+
+    type UnvalidatedOrder =
+        { OrderId: string
+          CustomerInfo: UnvalidatedCustomerInfo
+          ShippingAddress: UnvalidatedAddress
+          BillingAddress: UnvalidatedAddress
+          OrderLines: UnvalidatedOrderLine list }
+
+    and UnvalidatedCustomerInfo =
+        { FirstName: string
+          LastName: string
+          EmailAddress: string }
+
+    and UnvalidatedAddress = UnvalidatedAddress of string
+
+    and UnvalidatedOrderLine =
+        { ProductCode: string
+          OrderQuantity: string }
+
+    type ValidatedOrder =
+        { OrderId: OrderId
+          CustomerInfo: CustomerInfo
+          ShippingAddress: ShippingAddress
+          BillingAddress: BillingAddress
+          OrderLines: ValidatedOrderLine list }
+
+    and CustomerInfo =
+        { Name: PersonalName
+          EmailAddress: EmailAddress }
+
+    and PersonalName =
+        { FirstName: String50
+          LastName: String50 }
+
+    and ShippingAddress = private ShippingAddress of Address
+    and BillingAddress = private BillingAddress of Address
+    and ValidatedOrderLine = private ValidatedOrderLine of string
 
 module C0902 =
     let validateOrder
@@ -59,3 +118,49 @@ module C0902 =
 
     let validateOrder2: ValidateOrder =
         fun checkProductCodeExists checkAddressExists unvalidatedOrder -> failwith "Undefined"
+
+module C0903 =
+    open Domain
+    open Common
+
+    type UnvalidatedAddress = Undefined
+    type CheckedAddress = Undefined
+    type CheckAddressExists = UnvalidatedAddress -> CheckedAddress
+
+    type ProductCode = ProductCode of string
+    type CheckProductCodeExists = ProductCode -> bool
+
+    type ValidateOrder =
+        CheckProductCodeExists // dependency
+            -> CheckAddressExists // dependency
+            -> UnvalidatedOrder // In
+            -> ValidatedOrder // Out
+
+    let validateOrder: ValidateOrder =
+        let toCustomerInfo (customer: UnvalidatedCustomerInfo) =
+            let firstName = customer.FirstName |> String50.create
+            let lastName = customer.LastName |> String50.create
+            let emailAddress = customer.EmailAddress |> EmailAddress.create
+
+            { Name =
+                { FirstName = firstName
+                  LastName = lastName }
+              EmailAddress = emailAddress }
+
+        let toAddress _ = failwith "hoge"
+        let toOrderLines _ = failwith "hoge"
+
+        fun checkProductCodeExists checkAddressExists unvalidatedOrder ->
+
+            let orderId = unvalidatedOrder.OrderId |> OrderId.create
+            let customerInfo = unvalidatedOrder.CustomerInfo |> toCustomerInfo
+            let shippingAddress = unvalidatedOrder.ShippingAddress |> toAddress
+            let billingAddress = unvalidatedOrder.BillingAddress |> toAddress
+            let orderLines = unvalidatedOrder.OrderLines |> toOrderLines
+
+
+            { OrderId = orderId
+              CustomerInfo = customerInfo
+              ShippingAddress = shippingAddress
+              BillingAddress = billingAddress
+              OrderLines = orderLines }
