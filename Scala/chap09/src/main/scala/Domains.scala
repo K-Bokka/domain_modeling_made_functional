95966902c0671d05f1e79df1bda2086f358aa649
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-02-06 07:00:51 +0900

    Add OrderId value object with validation logic
    Introduce the OrderId case class and companion object to represent order identifiers. Added validation to ensure the value is non-empty and does not exceed 50 characters. Throws exceptions for invalid inputs to maintain data integrity.

--- Scala/chap09/src/main/scala/Domains.scala
+++ Scala/chap09/src/main/scala/Domains.scala
@@ -1,3 +1,11 @@
 object Domains:
+  case class OrderId private(value: String)
+
+  object OrderId:
+    def apply(value: String): OrderId =
+      if value.isEmpty
+      then throw new Exception("OrderId must not be empty")
+      else if value.length > 50 then throw new Exception("OrderId must not be more than 50 characters")
+      else new OrderId(value)
 
 end Domains
