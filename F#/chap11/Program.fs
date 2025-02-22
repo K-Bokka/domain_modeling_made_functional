2754a31a926e97a66d6efcc588093104da5fd1a8
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-02-22 10:17:54 +0900

    Transcribed 11.3 in F#

--- F#/chap11/Program.fs
+++ F#/chap11/Program.fs
@@ -1,1 +1,30 @@
 ﻿printfn "Chapter 11"
+
+module C1103 =
+    type MyInputType = Undefined
+    type MyOutputType = Undefined
+    type Workflow = MyInputType -> MyOutputType
+
+    type JsonString = string
+
+    type MyInputDto = Undefined
+    type DeserializeInputDto = JsonString -> MyInputDto
+    type InputDtoToDomain = MyInputDto -> MyInputType
+
+    type MyOutputDto = Undefined
+    type OutputDtoFromDomain = MyOutputType -> MyOutputDto
+    type SerializeOutputDto = MyOutputDto -> JsonString
+
+    let workflow _ = failwith "Not impl"
+    let deserializeInputDto _ = failwith "Not impl"
+    let inputDtoToDomain _ = failwith "Not impl"
+    let outputDtoFromDomain _ = failwith "Not impl"
+    let serializeOutputDto _ = failwith "Not impl"
+
+    let workflowWithSerialization jsonString =
+        jsonString
+        |> deserializeInputDto
+        |> inputDtoToDomain
+        |> workflow
+        |> outputDtoFromDomain
+        |> serializeOutputDto
