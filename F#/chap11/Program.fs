6250394b365e55ae6c7a0eb60d9667b068319a01
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-02-22 10:39:05 +0900

    Transcribed 11.4 in F#

--- F#/chap11/Program.fs
+++ F#/chap11/Program.fs
@@ -1,4 +1,6 @@
-﻿printfn "Chapter 11"
+﻿open System
+
+printfn "Chapter 11"
 
 module C1103 =
     type MyInputType = Undefined
@@ -28,3 +30,56 @@ module C1103 =
         |> workflow
         |> outputDtoFromDomain
         |> serializeOutputDto
+
+module C1104 =
+    type String50 = String50 of string
+
+    module String50 =
+        let create str = Ok(String50 str)
+        let value (String50 v) = v
+
+    type Birthdate = Birthdate of DateTime
+
+    module Birthdate =
+        let create bd = Ok(Birthdate bd)
+        let value (Birthdate v) = v
+
+    type ResultBuilder() =
+        member this.Return(x) = Ok x
+        member this.Bind(x, f) = Result.bind f x
+
+    let result = ResultBuilder()
+
+    module Domain =
+        type Person =
+            { First: String50
+              Last: String50
+              Birthdate: Birthdate }
+
+    module Dto =
+        type Person =
+            { First: string
+              Last: string
+              Birthdate: DateTime }
+
+    module Person =
+        let fromDomain (person: Domain.Person) : Dto.Person =
+            let first = person.First |> String50.value
+            let last = person.Last |> String50.value
+            let birthdate = person.Birthdate |> Birthdate.value
+
+            { First = first
+              Last = last
+              Birthdate = birthdate }
+
+        let toDomain (person: Dto.Person) : Result<Domain.Person, string> =
+            result {
+                let! first = person.First |> String50.create
+                let! last = person.Last |> String50.create
+                let! birthdate = person.Birthdate |> Birthdate.create
+
+                return
+                    { First = first
+                      Last = last
+                      Birthdate = birthdate }
+            }
