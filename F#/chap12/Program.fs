d1b5ba50473f22a2c35c6b1f5cc88ecc7078d042
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-02-25 06:29:30 +0900

    Transcribed 12.2.0 in F#

--- F#/chap12/Program.fs
+++ F#/chap12/Program.fs
@@ -2,6 +2,7 @@
 
 type Undefined = Undefined of string
 let notImplemented _ = failwith "Not impl"
+type AsyncResult<'success, 'failure> = Async<Result<'success, 'failure>>
 
 module C1201 =
     // 純粋ではない
@@ -72,3 +73,40 @@ module C1201 =
             markAsFullyPaidInDb invoiceId
             postInvoicePaidEvent invoiceId
         | PartiallyPaid updatedInvoice -> markAsPartiallyPaidInDb updatedInvoice
+
+module C1202 =
+    type DataStoreState = Undefined
+    type Data = Undefined
+    type NewDataStoreState = Undefined
+    type Query = Undefined
+    type Key = Undefined
+
+    module CQS =
+        type InsertData = DataStoreState -> Data -> NewDataStoreState
+        type ReadData = DataStoreState -> Query -> Data
+        type UpdateData = DataStoreState -> Data -> NewDataStoreState
+        type DeleteData = DataStoreState -> Key -> NewDataStoreState
+
+    type DbConnection = Undefined
+
+    module UseDbConnection =
+        type InsertData = DbConnection -> Data -> Unit
+        type ReadData = DbConnection -> Query -> Data
+        type UpdateData = DbConnection -> Data -> Unit
+        type DeleteData = DbConnection -> Key -> Unit
+
+    module DIDbConnection =
+        type InsertData = Data -> Unit
+        type ReadData = Query -> Data
+        type UpdateData = Data -> Unit
+        type DeleteData = Key -> Unit
+
+    type DbError = Undefined
+    type DbResult<'a> = AsyncResult<'a, DbError>
+
+    module UseAsyncResult =
+        type InsertData = Data -> DbResult<Unit>
+        type ReadData = Query -> DbResult<Data>
+        type UpdateData = Data -> DbResult<Unit>
+        type DeleteData = Key -> DbResult<Unit>
+        
